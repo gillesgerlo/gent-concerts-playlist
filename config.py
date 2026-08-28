d853e5d9b5a99f5119aa16a46a43f329fb1bee94
Gilles Gerlo <gillesgerlo@gmail.com>
2026-08-28 17:21:05 +0200

    Name change

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PLAYLIST_NAME = "Upcoming Concerts"
+PLAYLIST_NAME = "Upcoming Concerts Gent"
 CSV_PATH = Path("data/concerts.csv")
 HTML_PATH = Path("index.html")
 WINDOW_DAYS = 91
